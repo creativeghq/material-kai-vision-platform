4c8e23c1fb170eecb9892547befd7518b88fd93d
Basilakis <basiliskan@gmail.com>
2025-10-01 21:25:02 +0300

    updates pushed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
