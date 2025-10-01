00d2780e49fa4ebd8ce49d426ab12246a5cf2aa6
Basilakis <basiliskan@gmail.com>
2025-10-01 23:04:53 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic',
-      jsxImportSource: 'react'
-    })
-  ],
+  plugins: [react()],
   server: {
     port: 8080,
     host: true
