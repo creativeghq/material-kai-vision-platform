800569b6bb9cce4bee44d9d0244b2a7b99d07241
Basilakis <basiliskan@gmail.com>
2025-10-01 22:45:40 +0300

    updates

--- src/test.tsx
+++ src/test.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Test() {
   return (
     <div style={{ padding: '20px', fontSize: '24px', color: 'green' }}>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic'
+    })
+  ],
   server: {
     port: 8080,
     host: true
@@ -12,5 +16,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  esbuild: {
+    jsxInject: 'import React from "react"'
   }
 })
