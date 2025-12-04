39f4b6a4ca8933e0545e28f0c942120f9df0a86d
Basilakis <basiliskan@gmail.com>
2025-12-04 22:14:54 +0200

    feat: design review

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,20 @@
 // import React from 'react'; // Not needed for functional components
 
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
 import { Layout } from '@/components/Layout/Layout';
 import { Dashboard } from '@/components/Dashboard/Dashboard';
 
 const Index = () => {
   return (
     <Layout>
+      <div className="p-6 pb-0">
+        <Link to="/design-preview">
+          <Button className="w-full sm:w-auto">
+            View Design Preview
+          </Button>
+        </Link>
+      </div>
       <Dashboard />
     </Layout>
   );
