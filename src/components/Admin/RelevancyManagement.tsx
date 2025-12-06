f6fc0f19d68d7733bfeda9fdde43fa42d180b275
Basilakis <basiliskan@gmail.com>
2025-12-06 07:55:58 +0200

    fix: Remove max-w-7xl from RelevancyManagement to match other admin pages width

--- src/components/Admin/RelevancyManagement.tsx
+++ src/components/Admin/RelevancyManagement.tsx
@@ -384,7 +384,7 @@ export const RelevancyManagement: React.FC = () => {
       />
 
       {/* Main Content */}
-      <div className="max-w-7xl mx-auto px-6 py-6 space-y-6">
+      <div className="p-6 space-y-6">
         {/* Statistics Cards */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <Card>
